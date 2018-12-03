41b70ef8c3f0548c93bad0b516a9ff7f3cea6658
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-12-02 21:03:49 -0500

    Lambda list kinds: Add "operator".

--- lambda-list-kinds.lisp
+++ lambda-list-kinds.lisp
@@ -5,6 +5,8 @@
 (defclass fcll:standard-lambda-list-kind (fcll:lambda-list-kind)
   ((%name :initarg :name
           :reader name)
+   (%operator :initarg :operator
+              :reader operator)
    (%keywords :initarg :keywords
               :reader keywords)
    (%recurse :initarg :recurse
@@ -24,25 +26,30 @@
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :ordinary
+               :operator 'lambda
                :keywords '(:required &optional &rest &key &aux)) ;&allow-other-keys is subordinate to &key, so implied by it.
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :generic-function
+               :operator 'defgeneric
                :keywords (%derive-keywords-list :replace `((&optional ,(%no-defaulting '&optional))
                                                            (&key ,(%no-defaulting '&key)))
                                                 :remove '(&aux)))
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :specialized
+               :operator 'defmethod
                :keywords (%derive-keywords-list :replace '((:required :required-specializable))))
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :destructuring
+               :operator 'destructuring-bind
                :keywords (%derive-keywords-list :add '(&whole &body))
                :recurse :self)
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :macro
+               :operator 'defmacro
                :keywords (%derive-keywords-list :base :destructuring :add '(&environment))
                :recurse :destructuring)
 
@@ -50,18 +57,22 @@
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :defsetf
+               :operator 'defsetf
                :keywords (%derive-keywords-list :add '(&environment) :remove '(&aux)))
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :deftype
+               :operator 'deftype
                :keywords (%derive-keywords-list :base :macro)
                :recurse :destructuring
                :default '*)
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :define-modify-macro
+               :operator 'define-modify-macro
                :keywords (%derive-keywords-list :remove '(&key &aux)))
 
 (make-instance 'fcll:standard-lambda-list-kind
                :name :define-method-combination-arguments
+               :operator 'define-method-combination
                :keywords (%derive-keywords-list :add '(&whole)))
