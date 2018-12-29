1edcea773fa77a6866424f70d120f5d1626b6294
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-12-28 21:20:14 -0500

    Tests: Add tests for :destructuring, :macro, :boa and :defsetf.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -40,4 +40,103 @@
                 &optional optional1 optional2 optional3 optional4 (optional5 t) (optional6 nil optional6-supplied-p)
                 &rest rest
                 &key key1 key2 key3 (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
-                &aux aux1 aux2 aux3 (aux4 t))))
+                &aux aux1 aux2 aux3 (aux4 t)))
+  ;; "i-" as in "inner-"
+  (round-trip :destructuring
+              '(&whole whole
+                required1
+                (&whole i-whole
+                 i-required1
+                 &optional i-optional1 (i-optional2) (i-optional3 nil) (i-optional4 nil nil) (i-optional5 t) (i-optional6 nil i-optional6-supplied-p)
+                 &body i-rest
+                 &key i-key1 i-key2 ((:i-key3 i-key3)) (i-key4 t) (i-key5 nil i-key5-supplied-p) ((i-custom-key6 i-key6) nil i-key6-supplied-p) ((:i-custom-key7 i-key7)) ((i-key8 i-key8))
+                 &aux i-aux1 (i-aux2) (i-aux3 nil) (i-aux4 t))
+                &optional optional1 (optional2) (optional3 nil) (optional4 nil nil) (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 ((:key3 key3)) (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 (aux2) (aux3 nil) (aux4 t))
+              '(&whole whole
+                required1
+                (&whole i-whole
+                 i-required1
+                 &optional i-optional1 i-optional2 i-optional3 i-optional4 (i-optional5 t) (i-optional6 nil i-optional6-supplied-p)
+                 &body i-rest
+                 &key i-key1 i-key2 i-key3 (i-key4 t) (i-key5 nil i-key5-supplied-p) ((i-custom-key6 i-key6) nil i-key6-supplied-p) ((:i-custom-key7 i-key7)) ((i-key8 i-key8))
+                 &aux i-aux1 i-aux2 i-aux3 (i-aux4 t))
+                &optional optional1 optional2 optional3 optional4 (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 key3 (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 aux2 aux3 (aux4 t)))
+  (round-trip :macro
+              '(&whole whole
+                &environment env
+                required1
+                (&whole i-whole
+                 i-required1
+                 &optional i-optional1 (i-optional2) (i-optional3 nil) (i-optional4 nil nil) (i-optional5 t) (i-optional6 nil i-optional6-supplied-p)
+                 &body i-rest
+                 &key i-key1 i-key2 ((:i-key3 i-key3)) (i-key4 t) (i-key5 nil i-key5-supplied-p) ((i-custom-key6 i-key6) nil i-key6-supplied-p) ((:i-custom-key7 i-key7)) ((i-key8 i-key8))
+                 &aux i-aux1 (i-aux2) (i-aux3 nil) (i-aux4 t))
+                &optional optional1 (optional2) (optional3 nil) (optional4 nil nil) (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 ((:key3 key3)) (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 (aux2) (aux3 nil) (aux4 t))
+              '(&whole whole
+                &environment env
+                required1
+                (&whole i-whole
+                 i-required1
+                 &optional i-optional1 i-optional2 i-optional3 i-optional4 (i-optional5 t) (i-optional6 nil i-optional6-supplied-p)
+                 &body i-rest
+                 &key i-key1 i-key2 i-key3 (i-key4 t) (i-key5 nil i-key5-supplied-p) ((i-custom-key6 i-key6) nil i-key6-supplied-p) ((:i-custom-key7 i-key7)) ((i-key8 i-key8))
+                 &aux i-aux1 i-aux2 i-aux3 (i-aux4 t))
+                &optional optional1 optional2 optional3 optional4 (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 key3 (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 aux2 aux3 (aux4 t)))
+  (round-trip :macro
+              '(&whole whole
+                required1
+                &optional optional1 (optional2) (optional3 nil) (optional4 nil nil) (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 ((:key3 key3)) (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 (aux2) (aux3 nil) (aux4 t)
+                &environment env)
+              '(&whole whole
+                required1
+                &optional optional1 optional2 optional3 optional4 (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 key3 (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 aux2 aux3 (aux4 t)
+                &environment env))
+  (round-trip :boa
+              '(required1
+                &optional optional1 (optional2) (optional3 nil) (optional4 nil nil) (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 ((:key3 key3)) (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 (aux2) (aux3 nil) (aux4 t))
+              '(required1
+                &optional optional1 optional2 optional3 optional4 (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 key3 (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &aux aux1 aux2 aux3 (aux4 t)))
+  (round-trip :defsetf
+              '(required1
+                &optional optional1 (optional2) (optional3 nil) (optional4 nil nil) (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 ((:key3 key3)) (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8)))
+              '(required1
+                &optional optional1 optional2 optional3 optional4 (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 key3 (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))))
+  (round-trip :defsetf
+              '(required1
+                &optional optional1 (optional2) (optional3 nil) (optional4 nil nil) (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 ((:key3 key3)) (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &environment env)
+              '(required1
+                &optional optional1 optional2 optional3 optional4 (optional5 t) (optional6 nil optional6-supplied-p)
+                &rest rest
+                &key key1 key2 key3 (key4 t) (key5 nil key5-supplied-p) ((custom-key6 key6) nil key6-supplied-p) ((:custom-key7 key7)) ((key8 key8))
+                &environment env)))
